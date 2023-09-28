c1b13779f1ec000731fc1b3366f98f32a4b507f0
aloh596 <123131451+ColdToasty@users.noreply.github.com>
2023-09-29 00:24:04 +1300

    itemMenu fully working
    items amounts and presents amount will save when next is pressed
    
    Implemented short descriptions for each item and displays the current amount user has
    
    set limit to amount player can have and buy ~ 999
    
    Formatted presents display to be aligned with  amount display for items

--- UserInterface/playerMenu/itemMenu/itemMenu.cs
+++ UserInterface/playerMenu/itemMenu/itemMenu.cs
@@ -6,12 +6,12 @@ public class itemMenu : Control
 {
 
 	int ornamentCost = 3;
-	int tinselCost = 5;
-	int caneCost = 8;
+	int tinselCost = 7;
+	int caneCost = 15;
 	int invisibilityCost = 10;
 
 
-	Label ornamentLabel, ornamentCurrentLabel, tinselLabel, tinselCurrentLabel, caneLabel, caneCurrentLabel, invisibilityLabel, invisibilityCurrentLabel;
+	Label ornamentLabel, ornamentAmount, tinselLabel, tinselAmount, caneLabel, caneAmount, invisibilityLabel, invisibilityAmount, presentsLabel;
 	string currentString = "Current: ";
 
 	Stack<string> playerChoice = new Stack<string>();
@@ -24,10 +24,15 @@ public class itemMenu : Control
 
 	bool showItemDescription = false;
 
-	string ornamentDescription = "Roll these bulbs to distract people";
-	string tinselDescription = "Throw these to tangle";
-	string caneDescription = "Let them taste the Candy Cane!";
-	string invisibilityDescription = "Sneaky Santa";
+	string ornamentDescription = "Roll these at a certain direction and people will investigate";
+	string tinselDescription = "These will stop people from moving";
+	string caneDescription = "Will knock people out for a certain amount of time";
+	string invisibilityDescription = "Turns you invisible so people can't see you";
+
+	string currentDescription = "";
+
+	string saveName = GameFiles.save_name;
+
 
 	private enum ITEMHOVER
 	{
@@ -38,9 +43,9 @@ public class itemMenu : Control
 		INVISIBILITY
 	}
 
-	private ITEMHOVER hoveringOer = ITEMHOVER.NONE;
+	private ITEMHOVER hoveringOver = ITEMHOVER.NONE;
 
-    public override void _Ready()
+	public override void _Ready()
 	{
 		//Open save file and get amount of items
 		ornamentCount = playerData["ornamentCount"].ToString().ToInt();
@@ -56,31 +61,37 @@ public class itemMenu : Control
 		caneButton = GetNode<TextureButton>("VBoxContainer/cane/HBoxContainer/TextureButton");
 		invisibilityButton = GetNode<TextureButton>("VBoxContainer/invisibility/HBoxContainer/TextureButton");
 
+
+		presentsLabel = GetNode<Label>("VBoxContainer/title/HBoxContainer/HboxContainer/TextureRect2/Label");
+
 		ornamentLabel = GetNode<Label>("VBoxContainer/ornament/HBoxContainer/cost/Label");
-		ornamentCurrentLabel = GetNode<Label>("VBoxContainer/ornament/HBoxContainer/currentAmount/Label");
+		ornamentAmount = GetNode<Label>("VBoxContainer/ornament/HBoxContainer/amount/Label");
 
 		tinselLabel = GetNode<Label>("VBoxContainer/tinsel/HBoxContainer/cost/Label");
-		tinselCurrentLabel = GetNode<Label>("VBoxContainer/tinsel/HBoxContainer/currentAmount/Label");
+		tinselAmount = GetNode<Label>("VBoxContainer/tinsel/HBoxContainer/amount/Label");
 
 		caneLabel = GetNode<Label>("VBoxContainer/cane/HBoxContainer/cost/Label");
-		caneCurrentLabel = GetNode<Label>("VBoxContainer/cane/HBoxContainer/currentAmount/Label");
+		caneAmount = GetNode<Label>("VBoxContainer/cane/HBoxContainer/amount/Label");
 
 		invisibilityLabel = GetNode<Label>("VBoxContainer/invisibility/HBoxContainer/cost/Label");
-		invisibilityCurrentLabel = GetNode<Label>("VBoxContainer/invisibility/HBoxContainer/currentAmount/Label");
+		invisibilityAmount = GetNode<Label>("VBoxContainer/invisibility/HBoxContainer/amount/Label");
 
 		//Set the labels to reflect the cost of buying 1 item
 		ornamentLabel.Text = ornamentCost.ToString();
 		tinselLabel.Text = tinselCost.ToString();
 		caneLabel.Text = caneCost.ToString();
 		invisibilityLabel.Text = invisibilityCost.ToString();
+		
+		presentsLabel.Text = presentsCount.ToString();
 
 
 
-	}
+    }
 
 	private void _on_ornamentButton_pressed()
 	{
 		ornamentCount++;
+		presentsCount = presentsCount - ornamentCost;
 		playerChoice.Push("ornament");
 	}
 
@@ -88,6 +99,7 @@ public class itemMenu : Control
 	private void _on_tinselButton_pressed()
 	{ 
 		tinselCount++;
+		presentsCount = presentsCount - tinselCost;
 		playerChoice.Push("tinsel");
 	}
 
@@ -95,6 +107,7 @@ public class itemMenu : Control
 	private void _on_caneButton_pressed()
 	{
 		caneCount++;
+		presentsCount = presentsCount - caneCost;
 		playerChoice.Push("cane");
 	}
 
@@ -102,6 +115,7 @@ public class itemMenu : Control
 	private void _on_invisibilityButton_pressed()
 	{
 		invisibilityCount++;
+		presentsCount = presentsCount - invisibilityCost;
 		playerChoice.Push("invisibility");
 	}
 
@@ -113,18 +127,22 @@ public class itemMenu : Control
 		{
 			case "ornament":
 				ornamentCount--;
+				presentsCount = presentsCount + ornamentCost;
 				break;
 
 			case "tinsel":
 				tinselCount--;
+				presentsCount = presentsCount + tinselCost;
 				break;
 
 			case "cane":
 				caneCount--;
+				presentsCount = presentsCount + caneCost;
 				break;
 
 			case "invisibility":
 				invisibilityCount--;
+				presentsCount = presentsCount + invisibilityCost;
 				break;
 		}
 
@@ -136,6 +154,12 @@ public class itemMenu : Control
 	
 	private void _on_back_pressed()
 	{
+		presentsCount = playerData["currentPresents"].ToString().ToInt();
+		ornamentCount = playerData["ornamentCount"].ToString().ToInt();
+		tinselCount = playerData["tinselCount"].ToString().ToInt();
+		caneCount = playerData["caneCount"].ToString().ToInt();
+		invisibilityCount = playerData["invisibilityCount"].ToString().ToInt();
+		this.SetDeferred("visible", false);
 
 	}
 
@@ -143,7 +167,21 @@ public class itemMenu : Control
 	//Save counts and presents to save file
 	private void _on_next_pressed()
 	{
-	  
+		Godot.Collections.Dictionary<string, string> saveData = new Godot.Collections.Dictionary<string, string>();
+		playerData["ornamentCount"] = ornamentCount.ToString();
+		playerData["tinselCount"]= tinselCount.ToString();
+		playerData["caneCount"] = caneCount.ToString();
+		playerData["invisibilityCount"] = invisibilityCount.ToString();
+		playerData["currentPresents"]= presentsCount.ToString();
+
+		foreach(string key in playerData.Keys)
+		{
+			saveData.Add(key, playerData[key].ToString());
+		}
+
+		GameFiles.OnSaveGame(saveData);
+		this.SetDeferred("visible", false);
+
 	}
 
 
@@ -153,57 +191,88 @@ public class itemMenu : Control
 	{
 		GD.Print("hovering");
 		showItemDescription = !showItemDescription;
+		currentDescription = ornamentDescription;
+        hoveringOver = ITEMHOVER.ORNAMENT;
 
-    }
+	}
 
 	private void _on_ornamentButton_mouse_exited()
 	{
 		GD.Print("left");
-        showItemDescription = !showItemDescription;
-
-    }
+		showItemDescription = !showItemDescription;
+		currentDescription = "";
+        hoveringOver = ITEMHOVER.NONE;
+	}
 
 
 
 	private void _on_tinselButton_mouse_entered()
 	{
 
-        showItemDescription = !showItemDescription;
+		showItemDescription = !showItemDescription;
+        currentDescription = tinselDescription;
+        hoveringOver = ITEMHOVER.TINSEL;
 
-    }
+	}
 
 
 	private void _on_tinselButton_mouse_exited()
 	{
 
-        showItemDescription = !showItemDescription;
-
-    }
+		showItemDescription = !showItemDescription;
+        currentDescription = "";
+        hoveringOver = ITEMHOVER.NONE;
+	}
 
 
 	private void _on_caneButton_mouse_entered()
 	{
+		showItemDescription = !showItemDescription;
+        currentDescription = caneDescription;
+        hoveringOver = ITEMHOVER.CANE;
+	}
+
+
+
+    private void _on_caneButton_mouse_exited()
+    {
         showItemDescription = !showItemDescription;
+        currentDescription = "";
+        hoveringOver = ITEMHOVER.NONE;
     }
 
 
-	private void _on_invisibilitiyButton_mouse_entered()
+    private void _on_invisibilitiyButton_mouse_entered()
 	{
-        showItemDescription = !showItemDescription;
-    }
+		showItemDescription = !showItemDescription;
+        currentDescription = invisibilityDescription;
+        hoveringOver = ITEMHOVER.INVISIBILITY;
+	}
 
 
 	private void _on_invisibilityButton_mouse_exited()
 	{
-        showItemDescription = !showItemDescription;
-    }
+		showItemDescription = !showItemDescription;
+        currentDescription = "";
+        hoveringOver = ITEMHOVER.NONE;
+	}
 
 
 
 
 	public override void _Process(float delta)
 	{
-		if(playerChoice.Count > 0)
+
+		ornamentAmount.Text = ornamentCost.ToString();
+
+		tinselAmount.Text = tinselCount.ToString();
+
+		caneAmount.Text = caneCount.ToString();
+
+        invisibilityAmount.Text = invisibilityCount.ToString();
+
+
+        if (playerChoice.Count > 0)
 		{
 			undoButton.SetDeferred("disabled", false);
 		}
@@ -213,13 +282,11 @@ public class itemMenu : Control
 		}
 
 		//Update to display current amount user has
-		ornamentCurrentLabel.Text = currentString + ornamentCount.ToString();
-		tinselCurrentLabel.Text = currentString + tinselCount.ToString();
-		caneCurrentLabel.Text = currentString + caneCount.ToString();
-		invisibilityCurrentLabel.Text = currentString + invisibilityCount.ToString();
 
+		
+		presentsLabel.Text = presentsCount.ToString();
 
-		if(ornamentCost > presentsCount)
+		if (ornamentCost > presentsCount || ornamentCount > 998)
 		{
 			ornamentButton.SetDeferred("disabled", true);
 		}
@@ -228,7 +295,7 @@ public class itemMenu : Control
 			ornamentButton.SetDeferred("disabled", false);
 		}
 
-		if (tinselCost > presentsCount)
+		if (tinselCost > presentsCount || tinselCount > 998)
 		{
 			tinselButton.SetDeferred("disabled", true);
 		}
@@ -238,7 +305,7 @@ public class itemMenu : Control
 		}
 
 
-		if (caneCost > presentsCount)
+		if (caneCost > presentsCount || caneCount > 998)
 		{
 			caneButton.SetDeferred("disabled", true);
 		}
@@ -248,7 +315,7 @@ public class itemMenu : Control
 		}
 
 
-		if (invisibilityCount > presentsCount)
+		if (invisibilityCount > presentsCount || invisibilityCount > 998)
 		{
 			invisibilityButton.SetDeferred("disabled", true);
 		}
@@ -256,10 +323,36 @@ public class itemMenu : Control
 		{
 			tinselButton.SetDeferred("disabled", false);
 		}
+
+		//Displays a short description about an item
+		if (showItemDescription)
+		{
+            switch (hoveringOver)
+            {
+                case ITEMHOVER.ORNAMENT:
+                    break;
+
+                case ITEMHOVER.TINSEL:
+                    break;
+
+                case ITEMHOVER.CANE:
+                    break;
+
+                case ITEMHOVER.INVISIBILITY:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+
 	}
 
 
 }
+
 
 
 
